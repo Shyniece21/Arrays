e2c4a9b82b590b4b819829df107bbaefe2bff195
Alexis Jones <shyniece21@yahoo.com>
2022-12-07 00:18:37 -0700

    Update index position response

--- arrays.js
+++ arrays.js
@@ -1,10 +1,10 @@
-var family = ["Annette", "Alex Jr.", "Alex III", "Alexis"];
+var familyMembers = ["Annette", "Alex Jr.", "Alex III", "Alexis"];
 //Using the pop array method removes the last element from the array.
 family.pop();
 console.log(family);
 
 
-var number = [08, 06, 12, 04]
+var numbers = [08, 06, 12, 04]
 //Using the slice array method calls all elements in and between the index spots identified. In this case 1 through 4, which gives the output of 6, 12 and 4.
 console.log(number.slice (1, 4));
 
@@ -13,3 +13,6 @@ var questions = [true, false, false, true];
 //Using the unshift array method shifts elements to the beginning of the array. In this case an additonal true, false was added to the array after calling this method.
 questions.unshift(true, false);
 console.log(questions);
+
+
+//I utilized index positions in my number variable that I created above. When using the slice array method I had to know which position corresponded with the elements I wanted to access.
