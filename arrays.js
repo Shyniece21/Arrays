ed8c1e81d5514a56c21bc945daa4627b0676e21e
Alexis Jones <shyniece21@yahoo.com>
2022-12-07 00:07:54 -0700

    Update array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,15 @@
 var family = ["Annette", "Alex Jr.", "Alex III", "Alexis"];
+//Using the pop array method removes the last element from the array.
+family.pop();
+console.log(family);
+
+
 var number = [08, 06, 12, 04]
-var questions = [true, false, false, true]
+//Using the slice array method calls all elements in and between the index spots identified. In this case 1 through 4, which gives the output of 6, 12 and 4.
+console.log(number.slice (1, 4));
+
+
+var questions = [true, false, false, true];
+//Using the unshift array method shifts elements to the beginning of the array. In this case an additonal true, false was added to the array after calling this method.
+questions.unshift(true, false);
+console.log(questions);
